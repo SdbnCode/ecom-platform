850e90fa4ddc70cb6e5ef3ac32677ba321c6284d
Steven Nguyen <105184434+SdbnCode@users.noreply.github.com>
2024-12-17 14:56:56 -0500

    Create order page

---
+++ src/app/admin/orders/page.tsx
@@ -0,0 +1,80 @@
+import prisma from "@/lib/prisma";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { MoreVertical } from "lucide-react";
+import { DeleteDropDownItem } from "./_components/OrderActions";
+
+async function getOrders() {
+  return prisma.order.findMany({
+    select: {
+      id: true,
+      total: true,
+      product: { select: { name: true, description: true, image: true } },
+      user: { select: { email: true } },
+    },
+    orderBy: { createdAt: "desc" },
+  });
+}
+
+export default async function OrdersPage() {
+  return (
+    <>
+      <h1>Sales</h1>
+      <OrdersTable />
+    </>
+  );
+}
+
+async function OrdersTable() {
+  const orders = await getOrders();
+
+  if (orders.length === 0) {
+    return <div>No orders found.</div>;
+  }
+
+  return (
+    <Table>
+      <TableHeader>
+        <TableRow>
+          <TableHead>Product</TableHead>
+          <TableHead>Customer</TableHead>
+          <TableHead>Price Paid</TableHead>
+          <TableHead className="w-0">
+            <span className="sr-only">Actions </span>
+          </TableHead>
+        </TableRow>
+      </TableHeader>
+      <TableBody>
+        {orders.map((order) => (
+          <TableRow key={order.id}>
+            <TableCell>{order.product.name}</TableCell>
+            <TableCell>{order.user.email}</TableCell>
+            <TableCell>{order.total}</TableCell>
+            <TableCell className="text-center">
+              <DropdownMenu>
+                <DropdownMenuTrigger>
+                  <MoreVertical />
+                  <span className="sr-only">Actions</span>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DeleteDropDownItem id={order.id} />
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
